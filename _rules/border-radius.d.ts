5c86941abfc3f700abae238f337edb455112fbb7
MaikoTan <MaikoTan@users.noreply.github.com>
2024-06-25 03:29:46 +0000

    deploy: 3e98e843979034efa6f96f7958f05832e4127d11

--- _rules/border-radius.d.ts
+++ _rules/border-radius.d.ts
@@ -1,3 +1,4 @@
-import { Rule } from 'unocss';
+import { Rule, Shortcut } from 'unocss';
 
 export declare function getBorderRadiusRules(): Rule[];
+export declare function getBorderRadiusShortcuts(): Shortcut[];
